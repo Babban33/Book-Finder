21ca8b918cf5ba4ad564b05e9ee61fac3a27ca54
Chaitanya Wankhede <chaitanyawankhede705@gmail.com>
2024-11-08 23:45:40 +0530

    initial commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white">
+        <div className="container mx-auto text-center py-6">
+            <p className="text-sm">
+            Explore a wide variety of books and more.
+            </p>
+            <div className="text-sm mt-2 bg-[#f18e7d] py-2 px-4 inline-block rounded">
+                <a href="https://github.com/babban33">
+                Made with 💖 by Chaitanya Wankhede
+                </a>
+            </div>
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
